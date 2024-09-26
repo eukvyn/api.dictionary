eb721a1f6c4319b4d50f3ca351cca52606658bc6
Kevin Almeida <janioklalmeida05@gmail.com>
2024-09-26 01:05:06 -0300

    feat: create initial api routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,51 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return 'Fullstack Challenge 🏅 - Dictionary';
+});
+
+Route::middleware(['guest'])->group(function () {
+    Route::post('auth/signup', [RegisteredUserController::class, 'store'])
+        ->name('register');
+
+    Route::post('auth/signin', [AuthenticatedSessionController::class, 'store'])
+        ->name('login');
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('user/me', [UserController::class, 'me'])
+        ->name('register');
+});
+
+Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+    ->middleware('guest')
+    ->name('password.email');
+
+Route::post('/reset-password', [NewPasswordController::class, 'store'])
+    ->middleware('guest')
+    ->name('password.store');
+
+Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+    ->middleware(['auth', 'signed', 'throttle:6,1'])
+    ->name('verification.verify');
+
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+    ->middleware(['auth', 'throttle:6,1'])
+    ->name('verification.send');
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
