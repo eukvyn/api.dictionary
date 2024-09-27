ff9cbb6460e40a24b13a71b8f46066439791873f
Kevin Almeida <janioklalmeida05@gmail.com>
2024-09-27 17:13:26 -0300

    feat: new router groups and new entries/en route

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,16 +16,28 @@ Route::get('/', function () {
 });
 
 Route::middleware(['guest'])->group(function () {
-    Route::post('auth/signup', [RegisteredUserController::class, 'store'])
-        ->name('register');
+    Route::prefix('auth')->group(function () {
+        Route::post('/signup', [RegisteredUserController::class, 'store'])
+            ->name('register');
 
-    Route::post('auth/signin', [AuthenticatedSessionController::class, 'store'])
-        ->name('login');
+        Route::post('/signin', [AuthenticatedSessionController::class, 'store'])
+            ->name('login');
+
+        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+            ->name('logout');
+    });
 });
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('user/me', [UserController::class, 'me'])
-        ->name('register');
+    Route::prefix('user')->group(function () {
+        Route::get('/me', [UserController::class, 'me'])
+            ->name('profile');
+    });
+
+    Route::prefix('entries')->group(function () {
+        Route::get('/en', [WordController::class, 'index'])
+            ->name('index');
+    });
 });
 
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
@@ -42,10 +55,6 @@ Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
     ->middleware(['auth', 'throttle:6,1'])
     ->name('verification.send');
-
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('logout');
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
